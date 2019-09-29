9136392e5786f717c73339f67299a4b86b2468e3
Susan Busse <31662073+sbusse@users.noreply.github.com>
2019-09-29 18:03:39 -0500

    Add files via upload

---
+++ Week 6/Collections.playground/Collections.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var dodgeViper: Dictionary <String, String> = ["2005": "Yellow Coupe", "2006": "Blue-White Strip Coupe", "2007": "Red Convertible", "2008": "Red-White Stripe Coupe"]
+
+var dodgeViper2: [String: [Int]] = ["Blue": [2005, 2006, 2007], "Red": [2008, 2009, 2010]]
+
+printf_l(dodgeViper2 ["2005"] [])
